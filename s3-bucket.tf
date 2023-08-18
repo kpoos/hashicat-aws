dc2dd1e639d2774a7f47cbdcdd0e18b99fee26dd
Krisztian Poos <krisztian.poos@origoss.com>
2023-08-18 12:17:51 +0200

    s3

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,11 +1,4 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "s3-bucket"
-  acl    = "private"
-
-  versioning = {
-    enabled = true
-  }
-
+module "s3-bucket" {
+  source  = "app.terraform.io/kpoos-org/s3-bucket/aws"
+  version = "2.8.0"
 }
